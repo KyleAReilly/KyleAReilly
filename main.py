95c13f2146356fff939ace10e753e4758b06b15f
Kyle Reilly <Kyle.A.Reilly01@gmail.com>
2022-03-19 13:25:49 -0400

    Guessing Game

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}:  '))
+        print (guess)
+        if(guess < random_number):
+            print('THATS TOO LOW! Guess again!')
+        elif guess > random_number:
+            print('OPE too high friend! Guess again')
+    print(f'HUZZAH you got it ! {random_number} is correct')
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+             guess = low
+        guess = random.randint(low, high)
+        feedback = input(f"Is {guess} too high? (H), too low (L), or is it correct (C)").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'YAY, the robot guessed your number {guess}')
+
+
+computer_guess(100)
